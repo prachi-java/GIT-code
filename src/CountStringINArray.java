c99236ca35a6dfcd5006fb3925c9f4cbde3e64a5
mannu <mannu.tmh@gmail.com>
2021-03-09 16:57:04 -0600

    Third commit by Prachi

---
+++ src/CountStringINArray.java
@@ -0,0 +1,14 @@
+
+public class CountStringINArray {
+
+	public static void main(String[] args) {
+		String s = "HELLO WORLD HELLO";
+		
+		String str[] = s.split(" ");
+		int count = str.length;
+		System.out.println(count);
+		
+
+	}
+
+}
